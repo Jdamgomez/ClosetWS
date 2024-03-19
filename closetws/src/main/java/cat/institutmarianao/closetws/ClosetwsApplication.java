8527dd464c40b76c5ee2af295180e081c1bece0b
cfgs <cfgs@pc-info-host>
2024-03-19 20:00:08 +0100

    Commit inicial

---
+++ closetws/src/main/java/cat/institutmarianao/closetws/ClosetwsApplication.java
@@ -0,0 +1,13 @@
+package cat.institutmarianao.closetws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetwsApplication.class, args);
+	}
+
+}
---
+++ closetws/src/test/java/cat/institutmarianao/closetws/ClosetwsApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.institutmarianao.closetws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
