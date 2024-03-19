d90616862fd8f3c685177a5009ce70bf857df2ab
cfgs <cfgs@pc-info-host>
2024-03-19 20:51:16 +0100

    Iniciación clases

---
+++ closetws/src/main/java/cat/institutmarianao/closetws/exception/ForbiddenException.java
@@ -0,0 +1,5 @@
+package cat.institutmarianao.closetws.exception;
+
+public class ForbiddenException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
---
+++ closetws/src/main/java/cat/institutmarianao/closetws/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package cat.institutmarianao.closetws.exception;
+
+public class NotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+}
